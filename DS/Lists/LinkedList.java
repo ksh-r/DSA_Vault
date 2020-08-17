e48f52000f5bb2026630059d96ee00f6c57b0538
Keshar Wakharkar <keshar@Keshars-MacBook-Air.local>
2020-08-17 09:54:49 +0530

    Completed LinkedList

--- DS/Lists/LinkedList.java
+++ DS/Lists/LinkedList.java
@@ -21,6 +21,12 @@ public class LinkedList<T> {
         firstnode = null;
         size = 0;
     }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public int size(){
+        return size;
+    }
     public void add(T element, int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Wrong Index.");
@@ -44,6 +50,57 @@ public class LinkedList<T> {
              */
         }
         size++;
+    }
+    public T remove(int index){
+        if(index<0||index>size)
+            throw new IndexOutOfBoundsException("Wrong index.");
+        T element;
+        if(index == 0)
+        {
+            element = firstnode.data;
+            firstnode = firstnode.next;
+        }
+        else
+        {
+            LinkedListNode<T> p = firstnode;
+            for(int i=0;i<index-1;i++)
+            {
+                p = p.next;
+            }
+            element = p.next.data;
+            p.next = p.next.next;
+        }
+        size--;
+        return element;
+    }
+    public T get(int index){
+        if(index<0||index>size)
+            throw new IndexOutOfBoundsException("Wrong index.");
+        LinkedListNode<T> p = firstnode;
+        for(int i = 0; i < index; i++){
+            p = p.next;
+        }
+        return p.data;
+    }
+    public int getIndex(T element){
+        LinkedListNode<T> p = firstnode;
+        int index = 0;
+        while((p != null) && !(p.data.equals(element))){
+            p = p.next;
+            index++;
+        }
+        if(p == null)
+            return -1;
+        else
+            return index;
+    }
+    public void print(){
+        LinkedListNode<T> p = firstnode;
+        for(int i = 0; i < size; i++){
+            System.out.print(p.data + "  ");
+            p = p.next;
+        }
+        System.out.println();
     }
 }
 class LinkedListNode<T> {
